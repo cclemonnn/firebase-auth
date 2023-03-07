f9453f3f92eda48046f48fdc738bc8a4e9a7ce3c
cclemonnn <petersocool0313@gmail.com>
2023-03-08 05:37:49 +0800

    added refs to input

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,17 +1,28 @@
+import { useRef } from "react";
 import s from "./Signup.module.css";
 
 function Signup() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
   return (
     <div className={s.container}>
       <form className={s.signUpForm}>
         <div className={s.inputContainer}>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+          <input type="email" id="email" name="email" ref={emailRef} required />
         </div>
 
         <div className={s.inputContainer}>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            ref={passwordRef}
+            required
+          />
         </div>
 
         <div className={s.inputContainer}>
@@ -20,6 +31,7 @@ function Signup() {
             type="password"
             id="confirm-password"
             name="confirm-password"
+            ref={passwordConfirmRef}
             required
           />
         </div>
