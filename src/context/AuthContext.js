ebd133d370714e81ad19500324406b20ad3c669d
cclemonnn <petersocool0313@gmail.com>
2023-03-09 05:35:24 +0800

    added loading when onAuthStateChanged runs

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -13,6 +13,7 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
   const value = {
     currentUser,
     signUp,
@@ -22,7 +23,11 @@ export function AuthProvider({ children }) {
     // onAuthStateChanged is an event listener that listens for changes to the user authentication state, gets triggered whenever the authentication state changes (user logs in or out). When a change is detected, the callback function passed as the second argument is called, and it receives the user object as an argument.
     // When onAuthStateChanged gets called, it returns a unsubsribe function, which is stored in unsubscribe variable.
     const unsubscribe = onAuthStateChanged(auth, (user) => {
+      // user is an object with basic properties: email, displayName..
+      // https://firebase.google.com/docs/auth/web/manage-users
       setCurrentUser(user);
+      // wait until first user is set
+      setLoading(false);
     });
 
     // Calls the unsubscribe function when unmount.
@@ -34,5 +39,10 @@ export function AuthProvider({ children }) {
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={value}>
+      {/* does not render until onAuthStateChanged finishes */}
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
