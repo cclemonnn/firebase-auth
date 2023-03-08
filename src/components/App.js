4e452abb03dd4aaad8cc67636371fedc73d01ddd
cclemonnn <petersocool0313@gmail.com>
2023-03-09 06:38:58 +0800

    added BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 import { AuthProvider } from "../context/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
-      <Signup />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
