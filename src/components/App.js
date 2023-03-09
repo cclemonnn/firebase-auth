e665ee7aef16d490e79ebbc5373f28199ff986e0
cclemonnn <petersocool0313@gmail.com>
2023-03-10 04:48:16 +0800

    added ForgotPassword

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
+import ForgotPassword from "./ForgotPassword";
 import { AuthProvider } from "../context/AuthContext";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" element={<Dashboard />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
