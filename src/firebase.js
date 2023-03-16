eacfe7b964b3e1cce67e818c4eae342e7a9143b7
cclemonnn <petersocool0313@gmail.com>
2023-03-16 23:28:14 +0800

    added storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 export default app;
